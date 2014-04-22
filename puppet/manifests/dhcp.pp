12291d5f5fa4e6eefc86b50815ab39090dff9d63
Mark Lamourine <markllama@gmail.com>
2014-04-21 22:22:48 -0400

    start programming dhcp setup

---
+++ puppet/manifests/dhcp.pp
@@ -0,0 +1,47 @@
+#
+# Configure a DHCP server
+#
+
+package {dhcp:
+  ensure => present
+}
+
+service {dhcpd:
+  require => Package[dhcp] 
+
+}
+
+package {rsyslog:
+  ensure => present
+}
+
+service {rsyslog:
+  enable => true,
+  require => Package[rsyslog]
+}
+
+file {'/var/log/dhcpd.log':
+  ensure => present
+}
+
+
+file {'/etc/rsyslog.d/10-dhcpd.conf':
+  ensure => present,
+  content => 'if $programname == "dhcpd" then /var/log/dhcpd.log
+',
+  require => [Package[rsyslog], File['/var/log/dhcpd.log']]
+}
+
+file {'/etc/dhcp/dhcpd.conf':
+  content => '# LDAP backed DHCP config
+ldap-server "127.0.0.1" ;
+ldap-port 389 ;
+ldap-base-dn "dc=example,dc=com" ;
+ldap-dhcp-server-cn "dhcp" ;
+ldap-method dynamic ;
+ldap-debug-file "/var/log/dhcp-ldap-startup.log" ;
+ldap-username "cn=Manager,dc=example,dc=com" ;
+ldap-password "changeme" ;
+',
+  ensure => present
+}
