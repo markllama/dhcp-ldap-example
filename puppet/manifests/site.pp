866d685ff9f75fce8aa9cebb0f5412fe2adae897
Mark Lamourine <markllama@gmail.com>
2014-04-23 15:32:34 -0400

    adding service ldif definitions

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -48,33 +48,6 @@ service {'slapd':
 }
 
 
-#
-# Convert DHCP schema to LDIF
-#
-file { '/etc/openldap/schema/dhcp.schema':
-  ensure => present,
-  require => Package['dhcp']
-}
-
-# slapcat can convert configurations to LDIF format and print them
-# Load the DHCP schema and write it in LDIF format
-# then clean up
-exec { 'Convert DHCP schema to LDIF':
-  
-  command => "/usr/bin/echo 'include /etc/openldap/schema/dhcp.schema' > /tmp/slapd.conf-dhcp ; \
-mkdir -p /tmp/ldif_output ; \
-slapcat -f /tmp/slapd.conf-dhcp -F /tmp/ldif_output -n0 -H ldap:///cn={0}dhcp,cn=schema,cn=config -l /etc/openldap/schema/dhcp.ldif ; \
-sed -i -e '/CRC32/d ; s/{0}dhcp/dhcp/ ; /structuralObjectClass/,\$d' /etc/openldap/schema/dhcp.ldif; \
-rm -rf /tmp/ldif_output ; \
-rm /tmp/slapd.conf-dhcp ;
-  ",
-  creates => '/etc/openldap/schema/dhcp.ldif',
-  require => File['/etc/openldap/schema/dhcp.schema']
-}
-
-file {'/etc/openldap/schema/dhcp.ldif':
-  require => Exec['Convert DHCP schema to LDIF']
-}
 
 #
 # Load Cosine schema
@@ -104,14 +77,6 @@ exec {'Load Inet Org Person LDAP Schema':
 }
 
 
-#
-# Load DHCP schema
-#
-#exec {'Load DHCP LDAP Schema':
-#  command => '/usr/bin/sudo /usr/bin/ldapadd -Q -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/dhcp.ldif',
-#  unless => "/usr/bin/sudo /usr/bin/ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:/// -b 'cn=schema,cn=config' dn | grep -q dhcp,cn=schema",
-#  require => File['/etc/openldap/schema/dhcp.ldif']
-#}
 
 #
 # Set database base DN, root user, password
