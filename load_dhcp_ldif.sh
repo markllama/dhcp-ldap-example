0aa12f4755c5e17800b4ab317fa23c16d984f790
Mark Lamourine <markllama@gmail.com>
2014-05-09 14:18:24 -0400

    do all of ldif stuff

--- load_dhcp_ldif.sh
+++ load_dhcp_ldif.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+sudo systemctl restart rsyslog
+sudo cp dhcpd.conf /etc/dhcp
 
 CMD="ldapadd -x -w changeme -D cn=Manager,dc=example,dc=com"
 
 $CMD -f LDIF/dhcp-host.ldif
 $CMD -f LDIF/dhcp-service.ldif
 $CMD -f LDIF/dhcp-subnet-10.0.2.0.ldif
+
+sudo systemctl start dhcpd
+sudo systemctl enable dhcpd
